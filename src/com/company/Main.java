33d132e84e8536054f53556dee33bd59b8f03263
John McBride <jpmmcbride@gmail.com>
2017-08-29 16:22:35 -0600

    Initial commit
     Initial commit
    	new file:   src/com/company/Main.java
    	.idea/
    	CurrencyConversion.iml

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class US_Dollar {
+    double valuve = 1.00;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
